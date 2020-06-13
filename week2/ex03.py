6063b28969e68a04384f826e485e83ded9d4e511
ethanlabelle <ethan.labelle01@gmail.com>
2020-06-13 16:32:55 -0700

    ex03

---
+++ week2/ex03.py
@@ -0,0 +1,16 @@
+print('Please think of a number between 0 and 100!')
+min = 0
+max = 100
+guess = (int)((min + max) /2)
+print('Is your secret number {}?'.format(guess))
+ans = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
+while (ans != 'c'):
+    if ans == 'h':
+        max = guess
+    elif ans == 'l':
+        min = guess
+    guess = (int)((min + max) / 2)
+    print('Is your secret number {}?'.format(guess))
+    ans = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
+
+print('Game over. Your secret number was: {}'.format(guess))
